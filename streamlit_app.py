ead5cf5dd47371497178e8748c088561564d0094
lar4jx <113476950+lar4jx@users.noreply.github.com>
2026-08-29 23:24:35 +0000

    COm mudança consertada

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,19 +5,16 @@ st.write(
     "Saiba como seus parlamentares eleitos votam no Congresso Nacional"
 )
 lable = "Votação"
-if action == "PL do Estupro":
-    lable = "PL do Estupro"
-elif action == "PEC da Blindagem":
-    lable = "PEC da Blindagem"
-elif action == "PEC da Pedofilia":
-    lable = "PEC da Pedofilia"
 
 with st.container():
     st.write("Escolha a votação desejada")
 action = st.menu_button(lable, options=["PL do Estupro", "PEC da Blindagem", "PEC da Pedofilia"])
 if action == "PL do Estupro":
     st.write("Exporting as CSV...")
+    lable = "PL do Estupro"
 elif action == "PEC da Blindagem":
     st.write("Exporting as JSON...")
+    lable = "PEC da Blindagem"
 elif action == "PEC da Pedofilia":
     st.write("Exporting as PDF...")
+    lable = "PEC da Pedofilia"
