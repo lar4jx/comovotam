f775de0a9102d771cc33ca4a330ac75eb404b276
lar4jx <113476950+lar4jx@users.noreply.github.com>
2026-08-29 23:22:39 +0000

    Com mudança do botão de seleção

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,17 @@ st.title("Como votam?")
 st.write(
     "Saiba como seus parlamentares eleitos votam no Congresso Nacional"
 )
+lable = "Votação"
+if action == "PL do Estupro":
+    lable = "PL do Estupro"
+elif action == "PEC da Blindagem":
+    lable = "PEC da Blindagem"
+elif action == "PEC da Pedofilia":
+    lable = "PEC da Pedofilia"
+
 with st.container():
     st.write("Escolha a votação desejada")
-action = st.menu_button("Votação", options=["PL do Estupro", "PEC da Blindagem", "PEC da Pedofilia"])
+action = st.menu_button(lable, options=["PL do Estupro", "PEC da Blindagem", "PEC da Pedofilia"])
 if action == "PL do Estupro":
     st.write("Exporting as CSV...")
 elif action == "PEC da Blindagem":
