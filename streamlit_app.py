d354c75194f7adfb5de32eb3490ed7b2eae3f808
lar4jx <113476950+lar4jx@users.noreply.github.com>
2026-08-29 23:14:02 +0000

    Protótipo 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Como votam?")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Saiba como seus parlamentares eleitos votam no Congresso Nacional"
 )
+with st.container():
+    st.write("Escolha a votação desejada")
+action = st.menu_button("Votação", options=["PL do Estupro", "PEC da Blindagem", "PEC da Pedofilia"])
+if action == "PL do Estupro":
+    st.write("Exporting as CSV...")
+elif action == "PEC da Blindagem":
+    st.write("Exporting as JSON...")
+elif action == "PEC da Pedofilia":
+    st.write("Exporting as PDF...")
